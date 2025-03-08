ce7895226ff284a099207d166d45a29f311abd22
IrtyGo <77201294+irtygo@users.noreply.github.com>
2025-03-08 03:55:38 +0300

    Enable more stuff

--- src/config/general.h
+++ src/config/general.h
@@ -35,7 +35,7 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  */
 
 #define	NET_PROTO_IPV4		/* IPv4 protocol */
-//#define NET_PROTO_IPV6	/* IPv6 protocol */
+#define NET_PROTO_IPV6		/* IPv6 protocol */
 #undef	NET_PROTO_FCOE		/* Fibre Channel over Ethernet protocol */
 #define	NET_PROTO_STP		/* Spanning Tree protocol */
 #define	NET_PROTO_LACP		/* Link Aggregation control protocol */
@@ -113,20 +113,20 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
  *
  */
 //#define	IMAGE_NBI		/* NBI image support */
-//#define	IMAGE_ELF		/* ELF image support */
-//#define	IMAGE_MULTIBOOT		/* MultiBoot image support */
-//#define	IMAGE_PXE		/* PXE image support */
-//#define	IMAGE_SCRIPT		/* iPXE script image support */
-//#define	IMAGE_BZIMAGE		/* Linux bzImage image support */
-//#define	IMAGE_COMBOOT		/* SYSLINUX COMBOOT image support */
+#define	IMAGE_ELF			/* ELF image support */
+#define	IMAGE_MULTIBOOT			/* MultiBoot image support */
+#define	IMAGE_PXE			/* PXE image support */
+#define	IMAGE_SCRIPT			/* iPXE script image support */
+#define	IMAGE_BZIMAGE			/* Linux bzImage image support */
+#define	IMAGE_COMBOOT			/* SYSLINUX COMBOOT image support */
 //#define	IMAGE_EFI		/* EFI image support */
 //#define	IMAGE_SDI		/* SDI image support */
 //#define	IMAGE_PNM		/* PNM image support */
-#define	IMAGE_PNG		/* PNG image support */
-#define	IMAGE_DER		/* DER image support */
-#define	IMAGE_PEM		/* PEM image support */
-//#define	IMAGE_ZLIB		/* ZLIB image support */
-//#define	IMAGE_GZIP		/* GZIP image support */
+#define	IMAGE_PNG			/* PNG image support */
+#define	IMAGE_DER			/* DER image support */
+#define	IMAGE_PEM			/* PEM image support */
+#define	IMAGE_ZLIB			/* ZLIB image support */
+#define	IMAGE_GZIP			/* GZIP image support */
 //#define	IMAGE_UCODE		/* Microcode update image support */
 
 /*
@@ -154,24 +154,24 @@ FILE_LICENCE ( GPL2_OR_LATER_OR_UBDL );
 //#define DIGEST_CMD		/* Image crypto digest commands */
 //#define LOTEST_CMD		/* Loopback testing commands */
 //#define VLAN_CMD		/* VLAN commands */
-//#define PXE_CMD		/* PXE commands */
-//#define REBOOT_CMD		/* Reboot command */
-//#define POWEROFF_CMD		/* Power off command */
+#define PXE_CMD			/* PXE commands */
+#define REBOOT_CMD		/* Reboot command */
+#define POWEROFF_CMD		/* Power off command */
 //#define IMAGE_TRUST_CMD	/* Image trust management commands */
 //#define IMAGE_CRYPT_CMD	/* Image encryption management commands */
 //#define PCI_CMD		/* PCI commands */
 //#define PARAM_CMD		/* Request parameter commands */
 //#define NEIGHBOUR_CMD		/* Neighbour management commands */
-//#define PING_CMD		/* Ping command */
+#define PING_CMD		/* Ping command */
 //#define CONSOLE_CMD		/* Console command */
 //#define IPSTAT_CMD		/* IP statistics commands */
 //#define PROFSTAT_CMD		/* Profiling commands */
 //#define NTP_CMD		/* NTP commands */
 //#define CERT_CMD		/* Certificate management commands */
-//#define IMAGE_MEM_CMD		/* Read memory command */
+#define IMAGE_MEM_CMD		/* Read memory command */
 #define IMAGE_ARCHIVE_CMD	/* Archive image management commands */
 #define SHIM_CMD		/* EFI shim command (or dummy command) */
-//#define USB_CMD		/* USB commands */
+#define USB_CMD			/* USB commands */
 
 /*
  * ROM-specific options
